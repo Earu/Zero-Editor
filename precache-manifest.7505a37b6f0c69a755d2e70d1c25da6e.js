0dc40386e349a53e38ff66b004db77856350f10f
Earu <Earu@users.noreply.github.com>
2020-10-25 15:31:15 +0000

    deploy: 38f37b53594cc15b93a319ad2d39af713ec8e97f

---
+++ precache-manifest.7505a37b6f0c69a755d2e70d1c25da6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34744672115f9fa1eccd565ddb1b427b",
+    "url": "/Zero-Editor/index.html"
+  },
+  {
+    "revision": "034393ff462f22a1537d",
+    "url": "/Zero-Editor/static/css/main.e89adb7f.chunk.css"
+  },
+  {
+    "revision": "120729f215d655dc1677",
+    "url": "/Zero-Editor/static/js/2.6bdb0d49.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Zero-Editor/static/js/2.6bdb0d49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "034393ff462f22a1537d",
+    "url": "/Zero-Editor/static/js/main.11ebbc17.chunk.js"
+  },
+  {
+    "revision": "2a97090395eb11564055",
+    "url": "/Zero-Editor/static/js/runtime-main.24943b5a.js"
+  }
+]);
