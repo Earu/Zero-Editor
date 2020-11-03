dded2c692be3895417c14216aef2036d3a07600a
Earu <Earu@users.noreply.github.com>
2020-11-03 07:56:40 +0000

    deploy: e02410e71526b36eb6444c6c6b6a16abc1efb768

--- precache-manifest.69718cbdf75048ab308777e3431d3711.js
+++ precache-manifest.69718cbdf75048ab308777e3431d3711.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "465f648482442b4538bca76794012750",
+    "revision": "573bce3d4b045f56de9dda7b91246476",
     "url": "/Zero-Editor/index.html"
   },
   {
-    "revision": "1850f6be02b3723894a5",
+    "revision": "895c208df56942b9fcc6",
     "url": "/Zero-Editor/static/css/main.ebc88210.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Zero-Editor/static/js/2.890747d6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1850f6be02b3723894a5",
-    "url": "/Zero-Editor/static/js/main.7d5ba9fc.chunk.js"
+    "revision": "895c208df56942b9fcc6",
+    "url": "/Zero-Editor/static/js/main.9c94a68d.chunk.js"
   },
   {
     "revision": "2a97090395eb11564055",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Zero-Editor/precache-manifest.f36d324ab00c0c82ea5bc3052b22095f.js"
+  "/Zero-Editor/precache-manifest.69718cbdf75048ab308777e3431d3711.js"
 );
 
 self.addEventListener('message', (event) => {
