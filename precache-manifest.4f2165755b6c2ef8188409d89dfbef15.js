2f1a593cbd4954e77314fdc50219abdbcf2a4ef7
Earu <Earu@users.noreply.github.com>
2020-11-07 17:09:15 +0000

    deploy: 3f4f2d1723a20b3565bc5d7f9951823cd75cabfc

--- precache-manifest.4f2165755b6c2ef8188409d89dfbef15.js
+++ precache-manifest.4f2165755b6c2ef8188409d89dfbef15.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ab6840f78faea46388758a4c6839448b",
+    "revision": "26f20fff1c095495af076b84dafbaf2c",
     "url": "/Zero-Editor/index.html"
   },
   {
-    "revision": "2dccaf9c84b88fab4e9b",
+    "revision": "bb1c8cedf61bd28e9a23",
     "url": "/Zero-Editor/static/css/main.13500ce1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Zero-Editor/static/js/2.890747d6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2dccaf9c84b88fab4e9b",
-    "url": "/Zero-Editor/static/js/main.5d13f790.chunk.js"
+    "revision": "bb1c8cedf61bd28e9a23",
+    "url": "/Zero-Editor/static/js/main.2e2776ba.chunk.js"
   },
   {
     "revision": "2a97090395eb11564055",
